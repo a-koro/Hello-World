61ae8831960cb78e9947ac15e1e7c92eba358f9c
a-koro <korove@hotmail.com>
2019-12-03 09:11:16 +0200

    test git commit

--- helloScript.js
+++ helloScript.js
@@ -1,4 +1,3 @@
-//fdfdsfsdfs
 var inputAge = parseInt(window.prompt("Enter your age: "));
 if (inputAge >= 18){
     document.write("Hello, you are in a proper age.");
@@ -6,4 +5,3 @@ if (inputAge >= 18){
 else{
     document.write("You are a bit young.")
 }
-//test
